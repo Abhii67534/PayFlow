37383060efa176aae748654a1286b17dfacf00ff
Abhii67534 <154100444+Abhii67534@users.noreply.github.com>
2024-09-05 09:59:50 -0400

    Remove unnecessary files from paytm clone

--- frontend/paytm/src/components/Balance.jsx
+++
@@ -1,9 +0,0 @@
-
-export const Balance = ({amount}) => {
-    
-    return (
-        <div className="text-xl text-emerald-200 text-moderustic pb-5">
-            Your Balance : $ {amount}
-        </div>
-    );
-};
